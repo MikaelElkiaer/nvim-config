ad4c60863857d837dbad25ec6f053be5eb179305
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-07-15 19:59:42 +0200

    Add <leader>c buffer delete keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -36,7 +36,7 @@ keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 
 -- Close buffers
--- keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+keymap("n", "<leader>c", "<cmd>Bdelete!<CR>", opts)
 
 -- Better paste
 keymap("v", "p", '"_dP', opts)
