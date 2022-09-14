1f16ef2088878d35144f697d0b2f259ddc8773d8
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-09-14 15:03:02 +0200

    Set toggleterm default to non-float

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -12,7 +12,7 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = false,
 	persist_size = true,
-	direction = "float",
+  persist_mode = false,
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
@@ -21,16 +21,18 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-w>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-w>c', [[<C-\><C-n><C-W>c]], opts)
+	local opts = { noremap = true }
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-w>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-w>c", [[<C-\><C-n><C-W>c]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, direction = "float" })
 
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
@@ -39,4 +41,3 @@ end
 function _LAZYDOCKER_TOGGLE()
 	lazydocker:toggle()
 end
-
