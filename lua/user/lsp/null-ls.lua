53422a581a1791be284d3d872a89ff6a75277239
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-09-18 13:53:57 +0200

    Remove null-ls stylua

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -17,7 +17,6 @@ null_ls.setup {
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
     formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
     formatting.google_java_format,
     diagnostics.flake8,
     diagnostics.markdownlint,
