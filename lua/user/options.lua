29ae8c5a4c2e21f93e8d254aabfe2809552b2ce0
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-10-03 09:13:08 +0200

    Set correct Neovide font

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,7 +33,7 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "NotoMono NF:h10"               -- the font used in graphical neovim applications
+vim.opt.guifont = "Noto Sans Mono:h8"               -- the font used in graphical neovim applications
 vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
