303167ec53ea7f3073bd51502c754c72de2a809b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-11-30 19:35:59 +0100

    Enable lualine for toggleterm and TelescopePrompt

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -44,7 +44,7 @@ lualine.setup {
     theme = "auto",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard", "toggleterm", "TelescopePrompt" },
+    disabled_filetypes = { "alpha", "dashboard" },
     always_divide_middle = true,
   },
   sections = {
