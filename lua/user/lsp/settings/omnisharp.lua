c69a712354a203139ffcd30efd049f5a8622017a
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2022-11-30 19:36:38 +0100

    Fix non-working omnisharp after installing via mason

--- lua/user/lsp/settings/omnisharp.lua
+++ lua/user/lsp/settings/omnisharp.lua
@@ -3,7 +3,6 @@ return {
   handlers = {
     ["textDocument/definition"] = require('omnisharp_extended').handler,
   },
-  cmd = { "/usr/bin/omnisharp" },
   root_dir = function(fname)
     if fname:sub(- #".csx") == ".csx" then
       return require('lspconfig').util.path.dirname(fname)
