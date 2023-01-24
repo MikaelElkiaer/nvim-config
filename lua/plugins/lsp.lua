abbd42350c26ca43507698ec640d4217860133ca
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-24 21:39:12 +0100

    Clean up lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,47 +2,51 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      {
-        "Hoffs/omnisharp-extended-lsp.nvim",
-      },
+      "Hoffs/omnisharp-extended-lsp.nvim",
     },
-    opts = function(_, opts)
-      local configs = require("lspconfig.configs")
-      if not configs.helm_ls then
-        configs.helm_ls = {
-          default_config = {
-            cmd = { "helm_ls", "serve" },
-            filetypes = { "helm" },
-            root_dir = function(fname)
-              local util = require("lspconfig.util")
-              return util.root_pattern("Chart.yaml")(fname)
-            end,
-          },
-        }
-      end
-      require("lspconfig").helm_ls.setup({
-        cmd = { "helm_ls", "serve" },
-        filetypes = { "helm" },
-      })
-      local more_opts = {
-        autoformat = false,
-        ---@type lspconfig.options
-        servers = {
-          omnisharp = {
-            filetypes = { "cs", "csx" },
-            handlers = {
-              ["textDocument/definition"] = require("omnisharp_extended").handler,
-            },
-            root_dir = function(fname)
-              if fname:sub(- #".csx") == ".csx" then
-                return require("lspconfig").util.path.dirname(fname)
-              end
-              return vim.fn.getcwd()
-            end,
-          },
+    opts = {
+      autoformat = false,
+      ---@type lspconfig.options
+      servers = {
+        helm_ls = {
+          cmd = { "helm_ls", "serve" },
+          filetypes = { "helm" },
+          mason = false,
         },
+        omnisharp = {
+          filetypes = { "cs", "csx" },
+          root_dir = function(fname)
+            if fname:sub(- #".csx") == ".csx" then
+              return require("lspconfig").util.path.dirname(fname)
+            end
+            return vim.fn.getcwd()
+          end,
+        },
+      },
+      setup = {
+        helm_ls = function(_, _)
+          local configs = require("lspconfig.configs")
+          if not configs.helm_ls then
+            configs.helm_ls = {
+              default_config = {
+                cmd = { "helm_ls", "serve" },
+                filetypes = { "helm" },
+                root_dir = function(fname)
+                  local util = require("lspconfig.util")
+                  return util.root_pattern("Chart.yaml")(fname)
+                end,
+              },
+            }
+          end
+          return false
+        end,
+        omnisharp = function(_, opts)
+          opts.handlers = {
+            ["textDocument/definition"] = require('omnisharp_extended').handler,
+          }
+          return false
+        end
       }
-      vim.tbl_deep_extend("force", opts, more_opts)
-    end,
+    }
   },
 }
