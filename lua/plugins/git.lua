f56806fa081fb8ac3860257d690fae786c504fb0
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-26 12:41:31 +0100

    Add git-conflict keymap

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,12 +2,17 @@ return {
   -- grab VCS (GitHub etc.) links from current line
   {
     "valorl/vcslink.nvim",
-    cmd = { "VcsLinkCopy", "VcsLinkBrowse" }
+    cmd = { "VcsLinkCopy", "VcsLinkBrowse" },
   },
   -- navigation and simple view for git conflicts
   {
-    'akinsho/git-conflict.nvim',
-    cmd = { "GitConflictListQf" },
+    "akinsho/git-conflict.nvim",
+    cmd = { "GitConflictListQf", "GitConflictNextConflict", "GitConflictPrevConflict" },
+    keys = {
+      { "<leader>gC", "<cmd>GitConflictListQf<cr>", desc = "conflicts" },
+      { "]C", "<cmd>GitConflictNextConflict<cr>", desc = "next conflict" },
+      { "[C", "<cmd>GitConflictPrevConflict<cr>", desc = "previous conflict" },
+    },
     opts = true,
   },
 }
