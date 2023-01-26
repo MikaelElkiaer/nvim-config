483ea7611a86ead95594a1c8c6460068119cb1b2
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-26 20:40:10 +0100

    I like turtles

--- lua/plugins/fun.lua
+++ lua/plugins/fun.lua
@@ -1,9 +1,28 @@
 return {
   {
-    'eandrju/cellular-automaton.nvim',
+    "eandrju/cellular-automaton.nvim",
     cmd = { "CellularAutomaton" },
     keys = {
-      { "<leader>F", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "FML" }
-    }
+      { "<leader>F", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "FML" },
+    },
+  },
+  {
+    "tamton-aquib/duck.nvim",
+    keys = {
+      {
+        "<leader>z",
+        function()
+          require("duck").hatch("🐢", 0.8)
+        end,
+        desc = "hatch turtle",
+      },
+      {
+        "<leader>Z",
+        function()
+          require("duck").cook()
+        end,
+        desc = "cook turtle",
+      },
+    },
   },
 }
