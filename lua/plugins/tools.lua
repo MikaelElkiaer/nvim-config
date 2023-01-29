12f044ffe11cd5c1bd29f559bb69383c65074bc5
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-29 14:45:58 +0100

    Switch to self-made base64 plugin

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -46,4 +46,11 @@ return {
       },
     },
   },
+  {
+    "MikaelElkiaer/base64.nvim",
+    keys = {
+      { "<leader>Bd", '<cmd>lua require("base64").decode()<cr>', desc = "base64 decode", mode = "x" },
+      { "<leader>Be", '<cmd>lua require("base64").encode()<cr>', desc = "base64 encode", mode = "x" },
+    },
+  },
 }
