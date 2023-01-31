a2ed736d2310b50067aba590589a268aac74ade0
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-01-31 08:49:10 +0100

    Replace mini pairs and surround

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,12 +7,12 @@ return {
         local utils = require("auto-save.utils.data")
 
         return vim.fn.getbufvar(buf, "&modifiable") == 1
-            and utils.not_in(vim.fn.getbufvar(buf, "&filetype"), {})
-            and not string.match(vim.fn.getcwd(), "%/nvim%-config$")
-            and utils.not_in(vim.fn.expand("%:t"), {
-              "picom.conf",
-              "wezterm.lua",
-            })
+          and utils.not_in(vim.fn.getbufvar(buf, "&filetype"), {})
+          and not string.match(vim.fn.getcwd(), "%/nvim%-config$")
+          and utils.not_in(vim.fn.expand("%:t"), {
+            "picom.conf",
+            "wezterm.lua",
+          })
       end,
       execution_message = {
         message = function()
@@ -82,6 +82,40 @@ return {
       },
       auto_session_suppress_dirs = {
         "/home/*",
+      },
+    },
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function(_, opts)
+      local nvim_autopairs = require("nvim-autopairs")
+      nvim_autopairs.setup(opts)
+
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if not cmp_status_ok then
+        return
+      end
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+    opts = {
+      check_ts = true,
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    opts = {
+      keymaps = {
+        insert = nil,
+        insert_line = nil,
+        normal = "gzs",
+        normal_cur = "gzss",
+        normal_line = "gzS",
+        normal_cur_line = "gzSS",
+        visual = "gzs",
+        visual_line = "gzS",
+        delete = "gzd",
+        change = "gzc",
       },
     },
   },
--- lua/plugins/lazyvim-override.lua
+++ lua/plugins/lazyvim-override.lua
@@ -11,4 +11,24 @@ return {
     "neo-tree.nvim",
     enabled = false,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.surround",
+    enabled = false,
+  },
 }
