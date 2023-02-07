f9f7521b3769cd7bf506c18d5ea18d756e2d7b98
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-02-07 09:16:36 +0100

    Remove some LazyVim default keymaps
    Signed-off-by: Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override LazyVim defaults
+vim.keymap.del("t", "<esc><esc>")
+vim.keymap.del("n", "<leader>gG")
