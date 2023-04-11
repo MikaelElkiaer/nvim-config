349e48f5a37fd46ad326a0dd63145b6dc55826a7
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-04-11 16:22:43 +0200

    Add hush treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,23 @@
 return {
   {
-    'nvim-treesitter/playground',
+    "nvim-treesitter/nvim-treesitter",
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_configs.hush = {
+        install_info = {
+          url = "https://github.com/MikaelElkiaer/tree-sitter-hush",
+          files = { "src/parser.c" },
+          -- optional entries:
+          branch = "main", -- default branch in case of git repo if different from master
+          generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+        },
+      }
+    end,
+  },
+  {
+    "nvim-treesitter/playground",
     cmd = "TSPlaygroundToggle",
     opts = true,
   },
@@ -10,34 +27,46 @@ return {
     keys = {
       -- Normal Mode Swapping:
       -- Swap The Master Node relative to the cursor with it's siblings, Dot Repeatable
-      { "vU", function()
-        vim.opt.opfunc = "v:lua.STSSwapUpNormal_Dot"
-        return "g@l"
-      end },
-      { "vD", function()
-        vim.opt.opfunc = "v:lua.STSSwapDownNormal_Dot"
-        return "g@l"
-      end },
+      {
+        "vU",
+        function()
+          vim.opt.opfunc = "v:lua.STSSwapUpNormal_Dot"
+          return "g@l"
+        end,
+      },
+      {
+        "vD",
+        function()
+          vim.opt.opfunc = "v:lua.STSSwapDownNormal_Dot"
+          return "g@l"
+        end,
+      },
       -- Swap Current Node at the Cursor with it's siblings, Dot Repeatable
-      { "vd", function()
-        vim.opt.opfunc = "v:lua.STSSwapCurrentNodeNextNormal_Dot"
-        return "g@l"
-      end },
-      { "vu", function()
-        vim.opt.opfunc = "v:lua.STSSwapCurrentNodePrevNormal_Dot"
-        return "g@l"
-      end },
+      {
+        "vd",
+        function()
+          vim.opt.opfunc = "v:lua.STSSwapCurrentNodeNextNormal_Dot"
+          return "g@l"
+        end,
+      },
+      {
+        "vu",
+        function()
+          vim.opt.opfunc = "v:lua.STSSwapCurrentNodePrevNormal_Dot"
+          return "g@l"
+        end,
+      },
       -- Visual Selection from Normal Mode
-      { "vx", '<cmd>STSSelectMasterNode<cr>' },
-      { "vn", '<cmd>STSSelectCurrentNode<cr>' },
+      { "vx", "<cmd>STSSelectMasterNode<cr>" },
+      { "vn", "<cmd>STSSelectCurrentNode<cr>" },
       -- Select Nodes in Visual Mode
-      { "J", '<cmd>STSSelectNextSiblingNode<cr>', mode = "x" },
-      { "K", '<cmd>STSSelectPrevSiblingNode<cr>', mode = "x" },
-      { "H", '<cmd>STSSelectParentNode<cr>', mode = "x" },
-      { "L", '<cmd>STSSelectChildNode<cr>', mode = "x" },
+      { "J", "<cmd>STSSelectNextSiblingNode<cr>", mode = "x" },
+      { "K", "<cmd>STSSelectPrevSiblingNode<cr>", mode = "x" },
+      { "H", "<cmd>STSSelectParentNode<cr>", mode = "x" },
+      { "L", "<cmd>STSSelectChildNode<cr>", mode = "x" },
       -- Swapping Nodes in Visual Mode
-      { "<A-j>", '<cmd>STSSwapNextVisual<cr>', mode = "x" },
-      { "<A-k>", '<cmd>STSSwapPrevVisual<cr>', mode = "x" },
+      { "<A-j>", "<cmd>STSSwapNextVisual<cr>", mode = "x" },
+      { "<A-k>", "<cmd>STSSwapPrevVisual<cr>", mode = "x" },
     },
     opts = true,
   },
