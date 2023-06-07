33cd926e1a51f1887a43ac5a72d24ab7a3f8bda2
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-06-07 12:36:05 +0200

    Update copilot cmp config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -148,7 +148,35 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    opts = {},
+    config = function(_, opts)
+      require("copilot").setup(opts)
+
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("menu_opened", function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+
+        cmp.event:on("menu_closed", function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
+    end,
+    keys = {
+      {
+        "<leader>up",
+        "<cmd>Copilot toggle<cr>",
+        { desc = "Toggle Copilot" },
+      },
+    },
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = false,
+      },
+    },
   },
   {
     "axkirillov/hbac.nvim",
--- lua/plugins/lazyvim-override.lua
+++ lua/plugins/lazyvim-override.lua
@@ -15,6 +15,10 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       { "hrsh7th/cmp-nvim-lsp-signature-help" },
+      {
+        "zbirenbaum/copilot-cmp",
+        opts = {},
+      },
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
@@ -25,7 +29,10 @@ return {
         ["<C-k>"] = cmp.mapping.select_prev_item(),
       })
 
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "nvim_lsp_signature_help" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "copilot" },
+        { name = "nvim_lsp_signature_help" },
+      }))
     end,
   },
   {
