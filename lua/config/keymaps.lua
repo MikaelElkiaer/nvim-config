899fea02023b86fabeded2a5505b1ce79c3c947e
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-06-20 19:33:49 +0200

    Disable window navigation in term mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,7 @@
 -- Override LazyVim defaults
 vim.keymap.del("t", "<esc><esc>")
 vim.keymap.del("n", "<leader>gG")
+vim.keymap.del("t", "<c-h>")
+vim.keymap.del("t", "<c-j>")
+vim.keymap.del("t", "<c-k>")
+vim.keymap.del("t", "<c-l>")
