2788a29053ac8f3696fad9684c74137c66a9d52b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-09-18 08:35:54 +0200

    Add overseer

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -82,4 +82,20 @@ return {
       },
     },
   },
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      {
+        "<leader>rt",
+        "<cmd>OverseerRun<cr>",
+        desc = "Run task (Overseer)",
+      },
+      {
+        "<leader>rT",
+        "<cmd>OverseerToggle<cr>",
+        desc = "Toggle task list (Overseer)",
+      },
+    },
+    opts = {},
+  },
 }
