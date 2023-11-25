c89f6f6ef8df7457a31f072c77de2203e1a5d94b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2023-11-25 10:59:11 +0000

    Remove yaml.nvim

--- lua/plugins/programming.lua
+++ lua/plugins/programming.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "cuducos/yaml.nvim",
-    cmd = { "YAMLYank", "YAMLYankKey", "YAMLYankValue" },
-    config = function(_, _)
-      require("yaml_nvim")
-    end,
-    keys = {
-      { "<leader>yy", "<cmd>YAMLYank y<cr>", desc = "yank yaml key and value" },
-      { "<leader>yk", "<cmd>YAMLYankKey y<cr>", desc = "yank yaml key" },
-      { "<leader>yv", "<cmd>YAMLYankValue y<cr>", desc = "yank yaml value" },
-      { "<leader>yp", "\"yp", desc = "paste yaml value" },
-    },
-  },
-  {
     { "towolf/vim-helm", ft = "helm" },
   },
   -- generate c# xml doc
