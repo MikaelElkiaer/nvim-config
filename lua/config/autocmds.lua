40a98ec31ab0f52ca9ef1f49f6035339638c762f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-01-26 08:05:40 +0100

    Add autocmd for actionlint

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Run actionlint when changing a GitHub Actions workflow file
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    local lint, _ = pcall(require, "lint")
+    if not lint then
+      return
+    end
+    require("lint").try_lint("actionlint")
+  end,
+  pattern = "*.github/workflows/*.yaml",
+})
