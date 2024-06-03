464a94b08abe1965c0f3a0489475dcbb22bdc472
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-06-03 12:56:47 +0200

    Add more copilot keymaps

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -119,6 +119,22 @@ return {
         function()
           _ = require("copilot.suggestion").next()
         end,
+        desc = "Select next Copilot suggestion",
+        mode = "i",
+      },
+      {
+        "<M-j>",
+        function()
+          _ = require("copilot.suggestion").accept_word()
+        end,
+        desc = "Accept Copilot suggestion - word only",
+        mode = "i",
+      },
+      {
+        "<M-k>",
+        function()
+          _ = require("copilot.suggestion").prev()
+        end,
         desc = "Select previous Copilot suggestion",
         mode = "i",
       },
