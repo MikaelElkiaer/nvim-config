5589dc7d27fbc801d8fb3055a50c6840528f7c91
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-06-09 20:27:14 +0200

    Add octo.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -36,4 +36,15 @@ return {
       end,
     },
   },
+  {
+    "pwntester/octo.nvim",
+    -- cmd = { "Octa" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- OR 'ibhagwan/fzf-lua',
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
 }
