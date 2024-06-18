82b66e4e4288e3925586e09228239cba65868279
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-06-18 14:06:26 +0200

    Add better indent keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,6 @@
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv", { desc = "Indent decrease" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent increase" })
