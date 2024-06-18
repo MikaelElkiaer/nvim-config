0e0f3003b20d8a481af3645d6c79e864c00da9da
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-06-18 13:44:58 +0200

    Add missing base64 dependency

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -32,6 +32,7 @@ return {
   },
   {
     "MikaelElkiaer/base64.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
     keys = {
       { "<leader>Bd", '<cmd>lua require("base64").decode()<cr>', desc = "base64 decode", mode = "x" },
       { "<leader>Be", '<cmd>lua require("base64").encode()<cr>', desc = "base64 encode", mode = "x" },
