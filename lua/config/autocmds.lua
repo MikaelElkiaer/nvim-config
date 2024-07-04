101fd46a35952cd1f35da34139d942d33747b465
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-07-04 11:39:40 +0000

    linting: Drop actionlint autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,18 +6,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Run actionlint when changing a GitHub Actions workflow file
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    local success, lint = pcall(require, "lint")
-    if not success then
-      return
-    end
-    lint.try_lint("actionlint")
-  end,
-  pattern = "*.github/workflows/*.yaml",
-})
-
 -- Treat kubeconfigs as yaml
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   callback = function()
