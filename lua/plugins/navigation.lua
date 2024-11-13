17d6e2d43128a9a1de0512169dd94e457657117c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-11-13 13:39:55 +0100

    telescope: Add additional rg with no-ignore

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -7,7 +7,20 @@ return {
       { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "buffers" },
       { "<leader>ff", "<cmd>Telescope git_files show_untracked=true<cr>", desc = "files" },
       { "<leader>fF", "<cmd>Telescope find_files hidden=true<cr>", desc = "files - all" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "grep" },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep({ additional_args = { "--hidden" } })
+        end,
+        desc = "grep",
+      },
+      {
+        "<leader>fG",
+        function()
+          require("telescope.builtin").live_grep({ additional_args = { "--hidden", "--no-ignore" } })
+        end,
+        desc = "grep - no-ignore",
+      },
       { "<leader>fr", "<cmd>Telescope oldfiles only_cwd=true<cr>", desc = "recent files" },
       { "<leader>fR", "<cmd>Telescope oldfiles only_cwd=false<cr>", desc = "recent files - all" },
       { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "symbols" },
