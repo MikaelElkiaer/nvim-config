52aa15730d045991a8014c41d619e3aa93b8a10a
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2024-11-14 14:03:45 +0100

    keymap: Move completion out of lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,19 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent increase" })
 vim.keymap.set("n", "<leader>m", ":Man ", { desc = "Open manpage" })
 
 vim.keymap.set("n", "<leader>d", "<cmd>lua vim.diagnostic.open_float()<cr>", { desc = "Show diagnostics" })
+
+vim.keymap.set("i", "<C-space>", function()
+  return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-X><C-O>"
+end, { desc = "trigger completion", expr = true })
+vim.keymap.set("i", "<esc>", function()
+  return vim.fn.pumvisible() == 1 and "<C-e>" or "<esc>"
+end, { desc = "cancel completion", expr = true })
+vim.keymap.set("i", "<cr>", function()
+  return vim.fn.pumvisible() == 1 and "<C-y>" or "<cr>"
+end, { desc = "confirm completion", expr = true })
+vim.keymap.set("i", "<C-k>", function()
+  return vim.fn.pumvisible() == 1 and "<C-p>" or "<C-k>"
+end, { desc = "previous completion", expr = true })
+vim.keymap.set("i", "<C-j>", function()
+  return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-j>"
+end, { desc = "previous completion", expr = true })
