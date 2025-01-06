485450addb34d632b9864ac9ef48a68b7d2932ca
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-01-06 10:29:25 +0100

    ts: Add yaml block helm injections

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -10,3 +10,42 @@
     ; Skip initial `|` as this should not be part of the block
     ; - would cause stack overflow when injection language is "yaml"
     (#offset! @injection.content 0 1 0 0))
+
+; Inject language `helm` for `$tplYaml` blocks
+(block_mapping_pair
+  key: (flow_node) @_key
+  (#eq? @_key "$tplYaml")
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @injection.content)
+    (#set! injection.language "helm")))
+
+(block_mapping_pair
+  key: (flow_node) @_key
+  (#eq? @_key "$tplYaml")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "helm")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_key
+  (#eq? @_key "$tplYaml")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @injection.content))
+        (#set! injection.language "helm")))))
+
+(block_mapping_pair
+  key: (flow_node) @_key
+  (#eq? @_key "$tplYaml")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content
+          (#set! injection.language "helm")
+          (#offset! @injection.content 0 1 0 0))))))
