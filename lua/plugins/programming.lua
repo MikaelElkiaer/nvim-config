ccac4d67f6727d29c8c538778a1e19fc4c9231bd
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-01-10 08:21:08 +0100

    copilot: Align keymap with copilot chat

--- lua/plugins/programming.lua
+++ lua/plugins/programming.lua
@@ -18,23 +18,9 @@ return {
   },
   {
     "zbirenbaum/copilot.lua",
-    -- config = function(_, opts)
-    --   require("copilot").setup(opts)
-    --
-    --   local cmp_status_ok, cmp = pcall(require, "cmp")
-    --   if cmp_status_ok then
-    --     cmp.event:on("menu_opened", function()
-    --       vim.b.copilot_suggestion_hidden = true
-    --     end)
-    --
-    --     cmp.event:on("menu_closed", function()
-    --       vim.b.copilot_suggestion_hidden = false
-    --     end)
-    --   end
-    -- end,
     keys = {
       {
-        "<leader>uP",
+        "<leader>uC",
         "<cmd>Copilot toggle<cr>",
         desc = "Toggle Copilot",
       },
