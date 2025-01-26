1a84e74607f5425614554f4ccc7b5c7d8c7602fd
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-01-26 21:45:43 +0100

    render-markdown: Add

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -244,4 +244,17 @@ return {
       },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    cmd = { "RenderMarkdown" },
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    keys = {
+      { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
+    },
+    opts = {},
+  },
 }
