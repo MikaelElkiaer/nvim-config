0945d01f7dd1b2c031f98a0e47baab4e6e22a27b
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-02-12 12:11:11 +0100

    render-markdown: Disable by default

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -250,11 +250,13 @@ return {
     -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
     keys = {
       { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
     },
-    opts = {},
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      enabled = false,
+    },
   },
 }
