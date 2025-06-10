168587cf3a1a71eb77278c897db455832e4ae96f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-06-10 09:54:55 +0200

    buffers: Remove bufferline.nvim

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -2,31 +2,6 @@ require("utils.init"):create_keymap_group("<leader>b", "+buffers")
 
 return {
   {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    event = "BufEnter",
-    keys = {
-      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete others" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete left" },
-      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Buffer prev" },
-      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Buffer next" },
-      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Buffer move prev" },
-      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Buffer move next" },
-    },
-    opts = function()
-      local bufferline = require("bufferline")
-      return {
-        options = {
-          always_show_bufferline = true,
-          diagnostics = "nvim_lsp",
-          show_buffer_close_icons = false,
-          style_preset = bufferline.style_preset.no_italic,
-        },
-      }
-    end,
-  },
-  {
     "tiagovla/scope.nvim",
     config = true,
     event = "VeryLazy",
