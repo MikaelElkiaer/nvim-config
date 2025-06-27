1c041b08d1219146f8c114d4a894b78be61a8b82
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-06-27 11:04:17 +0200

    opt: Change diagnostic hint icon

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -54,7 +54,7 @@ vim.diagnostic.config({
     text = {
       [vim.diagnostic.severity.ERROR] = "󰅚",
       [vim.diagnostic.severity.WARN] = "󰀪",
-      [vim.diagnostic.severity.HINT] = "󰐙",
+      [vim.diagnostic.severity.HINT] = "",
       [vim.diagnostic.severity.INFO] = "",
     },
   },
