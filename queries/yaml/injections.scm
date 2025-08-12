a347c863968a469bd3f5ace239d99c6d628bc18d
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-08-12 22:01:12 +0200

    ts/queries: Generalize yaml patterns

--- queries/yaml/injections.scm
+++ queries/yaml/injections.scm
@@ -15,76 +15,56 @@
 (block_mapping_pair
   key: (flow_node) @_key
   (#eq? @_key "$tplYaml")
-  value: (flow_node
-    (plain_scalar
-      (string_scalar) @injection.content)
-    (#set! injection.language "helm")))
-
-(block_mapping_pair
-  key: (flow_node) @_key
-  (#eq? @_key "$tplYaml")
-  value: (block_node
-    (block_scalar) @injection.content
-    (#set! injection.language "helm")
-    (#offset! @injection.content 0 1 0 0)))
-
-(block_mapping_pair
-  key: (flow_node) @_key
-  (#eq? @_key "$tplYaml")
-  value: (block_node
-    (block_sequence
-      (block_sequence_item
-        (flow_node
-          (plain_scalar
-            (string_scalar) @injection.content))
-        (#set! injection.language "helm")))))
-
-(block_mapping_pair
-  key: (flow_node) @_key
-  (#eq? @_key "$tplYaml")
-  value: (block_node
-    (block_sequence
-      (block_sequence_item
-        (block_node
-          (block_scalar) @injection.content
-          (#set! injection.language "helm")
-          (#offset! @injection.content 0 1 0 0))))))
+  value: [
+    (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content)
+      (#set! injection.language "helm"))
+    (block_node
+      (block_scalar) @injection.content
+      (#set! injection.language "helm")
+      (#offset! @injection.content 0 1 0 0))
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content))
+          (#set! injection.language "helm"))))
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "helm")
+            (#offset! @injection.content 0 1 0 0)))))
+  ])
 
 ; Inject language `bash` for known CI files
 (block_mapping_pair
   key: (flow_node) @_run
   (#any-of? @_run "run" "script" "before_script" "after_script")
-  value: (flow_node
-    (plain_scalar
-      (string_scalar) @injection.content)
-    (#set! injection.language "bash")))
-
-(block_mapping_pair
-  key: (flow_node) @_run
-  (#any-of? @_run "run" "script" "before_script" "after_script")
-  value: (block_node
-    (block_scalar) @injection.content
-    (#set! injection.language "bash")
-    (#offset! @injection.content 0 1 0 0)))
-
-(block_mapping_pair
-  key: (flow_node) @_run
-  (#any-of? @_run "run" "script" "before_script" "after_script")
-  value: (block_node
-    (block_sequence
-      (block_sequence_item
-        (flow_node
-          (plain_scalar
-            (string_scalar) @injection.content))
-        (#set! injection.language "bash")))))
-
-(block_mapping_pair
-  key: (flow_node) @_run
-  (#any-of? @_run "script" "before_script" "after_script")
-  value: (block_node
-    (block_sequence
-      (block_sequence_item
-        (block_node
-          (block_scalar) @injection.content
-          (#set! injection.language "bash")
-          (#offset! @injection.content 0 1 0 0))))))
+  value: [
+    (flow_node
+      (plain_scalar
+        (string_scalar) @injection.content)
+      (#set! injection.language "bash"))
+    (block_node
+      (block_scalar) @injection.content
+      (#set! injection.language "bash")
+      (#offset! @injection.content 0 1 0 0))
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (flow_node
+            (plain_scalar
+              (string_scalar) @injection.content))
+          (#set! injection.language "bash"))))
+    (block_node
+      (block_sequence
+        (block_sequence_item
+          (block_node
+            (block_scalar) @injection.content
+            (#set! injection.language "bash")
+            (#offset! @injection.content 0 1 0 0)))))
+  ])
