5887748e1af1e7b00155dd904014bf0a4bba4bfb
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-09-10 20:39:36 +0200

    snacks: bufdelete

--- lua/plugins/buffers.lua
+++ lua/plugins/buffers.lua
@@ -1,63 +1,8 @@
-require("utils.init"):create_keymap_group("<leader>b", "+buffers")
 
 return {
   {
     "tiagovla/scope.nvim",
     config = true,
     event = "VeryLazy",
-  },
-  {
-    "nvim-mini/mini.bufremove",
-    keys = {
-      {
-        "<leader>bd",
-        function(...)
-          require("mini.bufremove").delete(...)
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete Buffer",
-      },
-      {
-        "<leader>bD",
-        function(...)
-          require("mini.bufremove").wipeout(...)
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete Buffer (Force)",
-      },
-      {
-        "<leader>ba",
-        function()
-          vim.cmd("bufdo bdelete")
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete buffers - All",
-      },
-      {
-        "<leader>bA",
-        function()
-          vim.cmd("bufdo bwipeout")
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete buffers - All (Force)",
-      },
-      {
-        "<leader>bo",
-        function()
-          vim.cmd(":execute 'bufdo if bufnr() != ' . bufnr('%') . ' | bdelete! | endif'")
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete buffers - Others",
-      },
-      {
-        "<leader>bO",
-        function()
-          vim.cmd(":execute 'bufdo if bufnr() != ' . bufnr('%') . ' | bwipeout! | endif'")
-          require("utils.init").on_buffer_delete()
-        end,
-        desc = "Delete buffers - Others (Force)",
-      },
-    },
-    opts = true,
   },
 }
--- lua/plugins/snacks.nvim.lua
+++ lua/plugins/snacks.nvim.lua
@@ -1,3 +1,4 @@
+require("utils.init"):create_keymap_group("<leader>b", "+buffers")
 local indent_ignored = {
   "help",
   "dashboard",
@@ -9,6 +10,57 @@ local indent_ignored = {
 
 return {
   "folke/snacks.nvim",
+  keys = {
+
+    {
+      "<leader>bd",
+      function()
+        require("snacks").bufdelete()
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffer",
+    },
+    {
+      "<leader>bD",
+      function()
+        require("snacks").bufdelete({ force = true })
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffer (force)",
+    },
+    {
+      "<leader>ba",
+      function()
+        require("snacks").bufdelete.all()
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffers - all",
+    },
+    {
+      "<leader>bA",
+      function()
+        require("snacks").bufdelete.all({ force = true })
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffers - all (force)",
+    },
+    {
+      "<leader>bo",
+      function()
+        require("snacks").bufdelete.other()
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffers - others",
+    },
+    {
+      "<leader>bO",
+      function()
+        require("snacks").bufdelete.other({ force = true })
+        require("utils.init").on_buffer_delete()
+      end,
+      desc = "Delete buffers - others (force)",
+    },
+  },
   lazy = false,
   ---@type snacks.Config
   opts = {
