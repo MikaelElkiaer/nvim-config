23d3c95bbd3332bec8e489f3d5c40a35d73c8d7c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-11-07 13:13:22 +0100

    toggleterm: Remove - use popupmux instead

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,31 +1,6 @@
 local M = {}
 
-M._terms = {}
 M._keymap_groups = {}
-
-M.create_tui = function(self, cmd)
-  local toggleterm = require("toggleterm.terminal")
-  local dir = vim.fn.getcwd()
-  local c = self._terms[cmd] or {}
-  c[dir] = c[dir]
-    or toggleterm.Terminal:new({
-      cmd = cmd,
-      dir = dir,
-      direction = "float",
-      hidden = true,
-      -- on_open = function(term)
-      --   vim.cmd("startinsert!")
-      --   -- stylua: ignore
-      --   -- Override q to keep TUI process alive
-      --   vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", '<cmd>close<cr>', { noremap = true, silent = true })
-      -- end,
-      -- on_close = function(_)
-      --   vim.cmd("startinsert!")
-      -- end,
-    })
-  c[dir]:toggle()
-  self._terms[cmd] = c
-end
 
 M.create_keymap_group = function(self, key, name)
   self._keymap_groups = self._keymap_groups or {}
