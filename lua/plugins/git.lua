5a138d9a4c4abc31b237d4baf54838c631c500f1
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-01-14 09:13:48 +0100

    gitsigns: Add hunk next/prev

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -110,6 +110,10 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = "BufEnter",
+    keys = {
+      { "]h", "<cmd>Gitsigns nav_hunk next<cr>", desc = "next hunk" },
+      { "[h", "<cmd>Gitsigns nav_hunk prev<cr>", desc = "prev hunk" },
+    },
     opts = {
       numhl = true,
       signcolumn = false,
