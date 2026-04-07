f7aee4ea26548321a087261ed6db71d069eecbd1
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-04-07 13:57:35 +0200

    treesitter: Remove unused

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,36 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
+    config = function()
+      -- source: https://github.com/MeanderingProgrammer/treesitter-modules.nvim
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("treesitter.setup", {}),
+        callback = function(args)
+          local buf = args.buf
+          local filetype = args.match
+
+          -- you need some mechanism to avoid running on buffers that do not
+          -- correspond to a language (like oil.nvim buffers), this implementation
+          -- checks if a parser exists for the current language
+          local language = vim.treesitter.language.get_lang(filetype) or filetype
+          if not vim.treesitter.language.add(language) then
+            return
+          end
+
+          -- replicate `fold = { enable = true }`
+          vim.wo.foldmethod = "expr"
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+          -- replicate `highlight = { enable = true }`
+          vim.treesitter.start(buf, language)
+
+          -- replicate `indent = { enable = true }`
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+          -- `incremental_selection = { enable = true }` covered by 0.12.0
+        end,
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
@@ -16,89 +46,6 @@ return {
           tsc.toggle()
         end,
         desc = "Toggle Treesitter Context",
-      },
-    },
-  },
-  {
-    "aaronik/treewalker.nvim",
-    cmd = "Treewalker",
-    keys = {
-      -- movement
-      { "<C-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "v" } },
-      { "<C-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "v" } },
-      { "<C-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "v" } },
-      { "<C-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "v" } },
-
-      -- swapping
-      { "<M-k>", "<cmd>Treewalker SwapUp<cr>", mode = { "n" } },
-      { "<M-j>", "<cmd>Treewalker SwapDown<cr>", mode = { "n" } },
-      { "<M-h>", "<cmd>Treewalker SwapLeft<cr>", mode = { "n" } },
-      { "<M-l>", "<cmd>Treewalker SwapRight<cr>", mode = { "n" } },
-    },
-    -- The following options are the defaults.
-    -- Treewalker aims for sane defaults, so these are each individually optional,
-    -- and setup() does not need to be called, so the whole opts block is optional as well.
-    opts = {
-      -- Whether to briefly highlight the node after jumping to it
-      highlight = true,
-
-      -- How long should above highlight last (in ms)
-      highlight_duration = 250,
-
-      -- The color of the above highlight. Must be a valid vim highlight group.
-      -- (see :h highlight-group for options)
-      highlight_group = "CursorLine",
-
-      -- Whether to create a visual selection after a movement to a node.
-      -- If true, highlight is disabled and a visual selection is made in
-      -- its place.
-      select = false,
-
-      -- Whether the plugin adds movements to the jumplist -- true | false | 'left'
-      --  true: All movements more than 1 line are added to the jumplist. This is the default,
-      --        and is meant to cover most use cases. It's modeled on how { and } natively add
-      --        to the jumplist.
-      --  false: Treewalker does not add to the jumplist at all
-      --  "left": Treewalker only adds :Treewalker Left to the jumplist. This is usually the most
-      --          likely one to be confusing, so it has its own mode.
-      jumplist = true,
-
-      -- Whether movement, when inside the scope of some node, should be confined to that scope.
-      -- When true, when moving through neighboring nodes inside some node, you won't be able to
-      -- move outside of that scope via :Treewalker Up/Down. When false, if on a node at the end
-      -- of a scope, movement will bring you to the next node of similar indentation/number of
-      -- ancestor nodes, even when it is outside of the scope you're currently in.
-      scope_confined = false,
-    },
-  },
-  {
-    "MeanderingProgrammer/treesitter-modules.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    lazy = false,
-    ---@module 'treesitter-modules'
-    ---@type ts.mod.UserConfig
-    opts = {
-      -- automatically install missing parsers when entering buffer
-      auto_install = true,
-      fold = {
-        enable = true,
-      },
-      highlight = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        -- set value to `false` to disable individual mapping
-        -- node_decremental captures both node_incremental and scope_incremental
-        keymaps = {
-          init_selection = "vn",
-          node_incremental = "<c-h>",
-          scope_incremental = "<c-k>",
-          node_decremental = "<c-l>",
-        },
-      },
-      indent = {
-        enable = true,
       },
     },
   },
