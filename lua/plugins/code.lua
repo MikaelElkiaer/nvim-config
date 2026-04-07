9b69373f0b4a4cb32e3624e417559a5c6953df4e
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-04-07 20:48:51 +0200

    Remove unused code plugins

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,32 +1,5 @@
 require("utils.init"):create_keymap_group("<leader>c", "+code")
 
-local function on_attach(client, bufnr)
-  local has_lspsaga = vim.fn.exists(":Lspsaga") == 2
-  local goto_definition = has_lspsaga and "<cmd>Lspsaga goto_definition<cr>"
-  local peek_definition = has_lspsaga and "<cmd>Lspsaga peek_definition<cr>"
-  local hover_doc = has_lspsaga and "<cmd>Lspsaga hover_doc<cr>"
-  local code_action = has_lspsaga and "<cmd>Lspsaga code_action<cr>" or vim.lsp.buf.code_action
-  local rename = has_lspsaga and "<cmd>Lspsaga rename<cr>" or vim.lsp.buf.rename
-
-  if client.server_capabilities.definitionProvider then
-    if goto_definition then
-      vim.keymap.set("n", "gd", goto_definition, { buffer = bufnr, desc = "definition" })
-    end
-    if peek_definition then
-      vim.keymap.set("n", "gD", peek_definition, { buffer = bufnr, desc = "definition - peek" })
-    end
-  end
-  if client.server_capabilities.codeActionProvider and code_action then
-    vim.keymap.set({ "n", "x" }, "<leader>ca", code_action, { buffer = bufnr, desc = "code actions" })
-  end
-  if client.server_capabilities.hoverProvider and hover_doc then
-    vim.keymap.set("n", "K", hover_doc, { buffer = bufnr, desc = "hover doc" })
-  end
-  if client.server_capabilities.renameProvider and rename then
-    vim.keymap.set("n", "<leader>cr", rename, { buffer = bufnr, desc = "rename" })
-  end
-end
-
 local function get_capabilities_factory()
   local blink_ok, blink = pcall(require, "blink.cmp")
   if blink_ok then
@@ -112,10 +85,9 @@ return {
       for server, config in pairs(opts.servers) do
         if type(config) == "function" then
           local capabilities = capabilities_factory({})
-          config = config({ lspconfig = { capabilities = capabilities, on_attach = on_attach } })
+          config = config({ lspconfig = { capabilities = capabilities } })
         else
           config.capabilities = capabilities_factory(config.capabilities)
-          config.on_attach = on_attach
         end
         vim.lsp.config(server, config)
         vim.lsp.enable(server)
@@ -179,33 +151,6 @@ return {
     },
   },
   {
-    "seblj/roslyn.nvim",
-    config = function(_, opts)
-      local capabilities_factory = get_capabilities_factory()
-      vim.lsp.config("roslyn", {
-        capabilities = capabilities_factory(opts.config and opts.config.capabilities),
-        cmd = {
-          "Microsoft.CodeAnalysis.LanguageServer",
-          "--logLevel=Information",
-          "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-          "--stdio",
-        },
-        on_attach = on_attach,
-        settings = {
-          ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-          },
-          ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-          },
-        },
-      })
-      require("roslyn").setup(opts)
-    end,
-    ft = "cs",
-  },
-  {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
     opts = {
@@ -218,50 +163,12 @@ return {
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
   {
-    "nvimdev/lspsaga.nvim",
-    cmd = "Lspsaga",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>cc", "<cmd>Lspsaga finder<cr>", desc = "lspsaga" },
-    },
-    opts = {
-      code_action = {
-        keys = {
-          quit = { "q", "<esc>" },
-        },
-      },
-      lightbulb = {
-        enable = false,
-      },
-      symbol_in_winbar = {
-        enable = false,
-      },
-    },
-  },
-  {
     "qvalentin/helm-ls.nvim",
     ft = "helm",
     opts = {
       conceal_templates = {
         -- enable the replacement of templates with virtual text of their current values
         enabled = false, -- tree-sitter must be setup for this feature
-      },
-    },
-  },
-  {
-    "danymat/neogen",
-    cmd = { "Neogen" },
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      languages = {
-        cs = {
-          template = {
-            annotation_convention = "xmldoc",
-          },
-        },
       },
     },
   },
