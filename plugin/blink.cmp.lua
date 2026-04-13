cc5a1b9c6722da3f5307987a2525616142efd55d
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-04-13 14:21:28 +0200

    blink.cmp: Lock version

--- plugin/blink.cmp.lua
+++ plugin/blink.cmp.lua
@@ -1,4 +1,9 @@
-vim.pack.add({ "https://github.com/saghen/blink.cmp" })
+vim.pack.add({
+  {
+    src = "https://github.com/saghen/blink.cmp",
+    version = vim.version.range("^1.0.0"),
+  },
+})
 
 require("blink.cmp").setup({
   keymap = {
