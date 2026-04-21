dbb27e89533f9e97e07e43cb351c14230569d875
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-04-21 13:10:17 +0200

    mini.ai: Update keymaps to not conflict with builtin

--- plugin/mini.ai.lua
+++ plugin/mini.ai.lua
@@ -5,4 +5,12 @@ vim.pack.add({
   },
 })
 
-require("mini.ai").setup({})
+require("mini.ai").setup({
+  -- Avoid conflict with incremental selection
+  mappings = {
+    around_next = "An",
+    inside_next = "In",
+    around_last = "Al",
+    inside_last = "Il",
+  },
+})
