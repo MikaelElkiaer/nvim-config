35d1bbc57bd1050d72148fd8830ef4a8cb1e637c
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-04-23 07:57:38 +0200

    oil: Add grep keymap

--- plugin/oil.nvim.lua
+++ plugin/oil.nvim.lua
@@ -12,6 +12,20 @@ vim.pack.add({
 })
 
 require("oil").setup({
+  keymaps = {
+    ["<localleader>fg"] = {
+      function()
+        local has_snacks, snacks = pcall(require, "snacks")
+        if has_snacks then
+          snacks.picker.grep({ dirs = { require("oil").get_current_dir() }, ignored = true, hidden = true })
+        else
+          vim.notify("Snacks not found", vim.log.levels.WARN)
+        end
+      end,
+      desc = "Grep - All",
+      mode = "n",
+    },
+  },
   view_options = {
     show_hidden = true,
   },
