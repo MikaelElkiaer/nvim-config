8183a1cbdeb4bafde0aac1e18e3dc13c063fc113
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-06-03 09:44:07 +0200

    mini.surround: Use gs prefixed keymaps

--- plugin/mini.surround.lua
+++ plugin/mini.surround.lua
@@ -7,11 +7,11 @@ vim.pack.add({
 
 require("mini.surround").setup({
   mappings = {
-    add = "Sa", -- Add surrounding in Normal and Visual modes
-    delete = "Sd", -- Delete surrounding
-    find = "Sf", -- Find surrounding (to the right)
-    find_left = "SF", -- Find surrounding (to the left)
-    highlight = "Sh", -- Highlight surrounding
-    replace = "Sr", -- Replace surrounding
+    add = "gsa", -- Add surrounding in Normal and Visual modes
+    delete = "gsd", -- Delete surrounding
+    find = "gsf", -- Find surrounding (to the right)
+    find_left = "gsF", -- Find surrounding (to the left)
+    highlight = "gsh", -- Highlight surrounding
+    replace = "gsr", -- Replace surrounding
   },
 })
