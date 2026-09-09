7105e222e16c1392fcfb14a8648c9731cb4b671f
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2026-09-09 12:42:22 +0200

    nvim-treesitter: Add tmux from source

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -33,3 +33,24 @@ vim.api.nvim_create_autocmd("FileType", {
     -- `incremental_selection = { enable = true }` covered by 0.12.0
   end,
 })
+
+-- Support for tmux was removed
+-- - see https://github.com/nvim-treesitter/nvim-treesitter/commit/78bebef150057eff7f26044bb177fbbf82ff047d
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    require("nvim-treesitter.parsers").tmux = {
+      tier = 0,
+      install_info = {
+        url = "https://github.com/Freed-Wu/tree-sitter-tmux",
+        revision = "58147321fa1f00daec15dd4d371bc9e2e9373459", -- commit hash for revision to check out; HEAD if missing
+        -- optional entries:
+        -- branch = 'develop', -- only needed if different from default branch
+        -- location = 'parser', -- only needed if the parser is in subdirectory of a "monorepo"
+        generate = true, -- only needed if repo does not contain pre-generated `src/parser.c`
+        generate_from_json = false, -- only needed if repo does not contain `src/grammar.json` either
+        queries = "queries", -- also install queries from given directory
+      },
+    }
+  end,
+})
